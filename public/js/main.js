167a08a7277196aceb5564df5a100062178d6c42
Aditya anand <adityaanandofficial@gmail.com>
2020-10-28 14:33:55 +0530

    securing

--- public/js/main.js
+++ public/js/main.js
@@ -13,7 +13,7 @@ const getInfo = async(event) => {
         datahide.classList.add("data_hide");
     } else {
         try {
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=950f5db1d05f814bc3808d3822f8b0de`
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=950f5db1d05f814bc3808d3822f8b0de`
             const response = await fetch(url);
             const data = await response.json();
 
